b98d4952f9e499e360e4210c459804c0c0d15da8
Duncan <duncanvdw@gmail.com>
2019-03-19 23:14:55 +0000

    Basic structure from practicals

--- cs3524/solutions/mud/Edge.java
+++ cs3524/solutions/mud/Edge.java
@@ -1,3 +1,9 @@
+/*
+*   Author:     Duncan van der Wielen
+*   Course:     CS3524 Distributed Systems and Security
+*   Content:    In-course assessment (MUD game)
+*/
+
 /***********************************************************************
  * cs3524.solutions.mud.Edge
  ***********************************************************************/
