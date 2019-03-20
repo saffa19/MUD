fa2411526506df76b3073229b8e234b4f059acc9
Duncan <duncanvdw@gmail.com>
2019-03-20 00:08:47 +0000

    Initial server structure from rmishout

--- cs3524/solutions/mud/MUDServer.java
+++ cs3524/solutions/mud/MUDServer.java
@@ -5,3 +5,49 @@
 */
 
 package cs3524.solutions.mud;
+
+import java.rmi.Naming;
+import java.rmi.server.UnicastRemoteObject;
+import java.lang.SecurityManager;
+import java.net.InetAddress;
+
+public class MUDServer {
+	public static void main(String args[]) {
+
+		if (args.length < 2) {
+			System.err.println( "Usage:\njava ShoutServerMainline <registryport> <serverport>" ) ;
+			return;
+		}
+
+		try {
+			String hostname = (InetAddress.getLocalHost()).getCanonicalHostName() ;
+			int registryport = Integer.parseInt( args[0] ) ; 	// specify which port the rmregistry is listening at for binding and lookup requests
+			int serverport = Integer.parseInt( args[1] ); 		// this is the port for our shout service
+			System.out.println("Server created!");
+
+			// This is the server's security policy
+			System.setProperty( "java.security.policy", "mud.policy" ) ;
+			System.setSecurityManager( new SecurityManager() ) ;
+
+			// These are the remote objects that will reside in the server
+			MUDServerImpl mudserver = new MUDServerImpl();
+			MUDServerInterface mudstub = (MUDServerInterface)UnicastRemoteObject.exportObject( mudserver, serverport );
+
+			String regURL = "rmi://" + hostname + ":" + registryport + "/MUDServer";
+			System.out.println("Registering " + regURL );
+			Naming.rebind( regURL, mudstub );
+
+			// Note the server will not shut down!
+		}
+
+		catch (java.net.UnknownHostException e) {
+			System.err.println( "Cannot get local host name." );
+			System.err.println( e.getMessage() );
+		}
+
+		catch (java.io.IOException e) {
+				System.err.println( "Failed to register." );
+				System.err.println( e.getMessage() );
+			}
+	}
+}
