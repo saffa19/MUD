7fc5cac9368b41026fbd0c928bca1cf74fc3ac22
Duncan <duncanvdw@gmail.com>
2019-03-20 21:47:54 +0000

    start() method welcomess player

--- cs3524/solutions/mud/MUDServerInterface.java
+++ cs3524/solutions/mud/MUDServerInterface.java
@@ -11,7 +11,7 @@ import java.rmi.RemoteException;
 
 // Interface provides access to methods in MUDServerImpl.java
 public interface MUDServerInterface extends Remote {
-
+	public String start() throws RemoteException; 					            			// misc
 	public void createMUD(String name) throws RemoteException; 					            // D3 - D1
 	public String moveThing(String loc, String dir, String thing) throws RemoteException;   // D3 - D1
 	public void delThing( String loc, String thing ) throws RemoteException;         		// C3 - C1
